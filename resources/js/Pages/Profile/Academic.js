e3b32c0b74541a30e8aee58ab47a51e6f3e12ff3
izmeclark <98464661+clark-jrb@users.noreply.github.com>
2024-05-03 13:24:42 +0800

    academic design

--- resources/js/Pages/Profile/Academic.js
+++ resources/js/Pages/Profile/Academic.js
@@ -18,9 +18,62 @@ export default function Academic(){
 
     return (
         <Profile>
-            <div className="p-3 academic-content">
-                <div className="title-info-content pb-2">
-                    <p className="m-0">Academic</p>
+            <div className="p-3 px-4 academic-content d-flex gap-5">
+                <div className="acad-educ-content flex-fill w-50">
+                    <div className="title-info-content-acad pb-2">
+                        <p className="m-0">Education</p>
+                    </div>
+                    {acadEduc_data.map((educ, index) => (
+                        <div className="p-3" key={index} style={{ borderBottom: '#ccc 1px solid' }}>
+                            <div className="acad-row1">
+                                <i className="fa-solid fa-graduation-cap" style={{ color: 'var(--light-green)' }}></i>
+                                &nbsp;
+                                <p className="m-0">{educ.degree}</p>
+                            </div>
+                            <div className="acad-row2">
+                                <p className="m-0">{educ.institution}</p>
+                            </div>
+                            <div className="d-flex gap-3">
+                                <div>
+                                    <i className="fa-regular fa-calendar-check"></i>
+                                    &nbsp;
+                                    {educ.date}
+                                </div>
+                                <div>
+                                    <i className="fa-solid fa-location-dot"></i>
+                                    &nbsp;
+                                    {educ.location}
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div className="acad-work-content flex-fill w-50">
+                    <div className="title-info-content-acad pb-2">
+                        <p className="m-0">Work Experience</p>
+                    </div>
+                    {acadWork_data.map((work, index) => (
+                        <div className="p-3" key={index} style={{ borderBottom: '#ccc 1px solid' }}>
+                            <div className="acad-row1">
+                                <p className="m-0">{work.position}</p>
+                            </div>
+                            <div className="acad-row2">
+                                <p className="m-0">{work.location}</p>
+                            </div>
+                            <div className="d-flex gap-3">
+                                <div>
+                                    <i className="fa-regular fa-calendar-check"></i>
+                                    &nbsp;
+                                    {work.date}
+                                </div>
+                                <div>
+                                    <i className="fa-solid fa-location-dot"></i>
+                                    &nbsp;
+                                    {work.work_loc}
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </Profile>
