e4b87ec839f4ba90aa428ecf5b3b285ec606c14e
BlitzkriegTactic <bermudezdarrenjasper@gmail.com>
2024-05-17 10:03:57 +0800

    added trainings

---
+++ resources/js/Pages/Profile/Trainings.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Profile from '../Trainings';
+
+export default function Trainings(){
+    return (
+        <Profile>
+            <div className="p-3 trainings-content">
+                <div className="title-info-content pb-2">
+                    <p className="m-0">Trainings</p>
+                </div>
+            </div>
+        </Profile>
+    )
+}
